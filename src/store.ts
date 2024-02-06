c72d9475c1cf2e43da69f6b62a9dce79878a2933
Fede Krenn <fedekrenn@gmail.com>
2024-02-06 20:04:25 -0300

    Refactor store.ts and add new text files

--- src/store.ts
+++ src/store.ts
@@ -7,12 +7,8 @@ export const APP_STATUS = {
   ERROR: -1
 }
 
-export const appStatus = writable(APP_STATUS.CHAT_MODE)
-export const appStatusInfo = writable({
-  id: 'c1a098ffcb49079c8180b18c7b15229a',
-  url: 'https://res.cloudinary.com/midudev/image/upload/v1706810969/pdf/khiice5vqnr1gcn1pmtq.pdf',
-  pages: 4
-})
+export const appStatus = writable(APP_STATUS.INIT)
+export const appStatusInfo = writable({ id: '', url: '', pages: 0 })
 
 export const setAppStatusLoading = () => {
   appStatus.set(APP_STATUS.LOADING)
@@ -23,8 +19,8 @@ export const setAppStatusError = () => {
 }
 
 export const setAppStatusChatMode = (
-  { id, url, pages } :
-  { id: string, url: string, pages: number }) => {
+  { id, url, pages }:
+    { id: string, url: string, pages: number }) => {
   appStatus.set(APP_STATUS.CHAT_MODE)
   appStatusInfo.set({ id, url, pages })
 }
